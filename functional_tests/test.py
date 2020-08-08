953115f469e31ce330490871df46749e4a8ebaae
Jonathan Haigh <jonathanhaigh0@gmail.ocm>
2020-08-08 21:43:01 +0100

    cv button styling and functional test hell

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -46,24 +46,82 @@ class AdminVisitorTest(LiveServerTestCase):
     def setUp(self):
         User.objects.create_superuser(
             username='admin',
-            password='admin',
+            password='psw',
             email='admin@example.com'
         )
 
         self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
+
+    def checkIsIn(self, element_list, phrase):
+        for element in element_list:
+            if element.text == phrase:
+                return true
+
+        return false
+
+    def test_can_add_education(self):
+        #go to admin login page and log in
         self.browser.get('%s%s' % (self.live_server_url,  "/admin/"))
 
         username = self.browser.find_element_by_id("id_username")
         username.send_keys("admin")
         password = self.browser.find_element_by_id("id_password")
-        password.send_keys("admin")
+        password.send_keys("psw")
+        log_in = self.browser.find_element_by_xpath('//input[@value="Log in"]')
+        log_in.click()
 
+        #back to main page
         self.browser.get(self.live_server_url)
+        time.sleep(10)
 
+        #Goes to CV page
+        cv_button = self.browser.find_element_by_id('id_cv_button')
+        cv_button.click()
+        current_url = self.browser.current_url
+        self.assertRegex(current_url, '.+/cv/')
 
-    def tearDown(self):
-        self.browser.quit()
+        #500 error on the cv page and i have no idea why
+        #spent about a week trying to fix it and i've now given up 
 
-    def test_can_add_education(self):
-        time.sleep(2)
+        #decides to add new education entry
+        time.sleep(10)
+        new_dated_button = self.browser.find_element_by_id('new-dated-btn')
+        new_dated_button.click()
+
+        #taken to a form page
+        current_url = self.browser.current_url
+        self.assertRegex(current_url, '.+/cv/new_dated')
+
+        #enters info (leaving the ended box unchecked)
+        title  = "Test University"
+        start_date = "01/07/2018"
+        text = "I really enjoyed my time here"
+        type_id = "education uni"
+
+        title_box = self.browser.find_element_by_id('id_title')
+        title_box.send_keys(title)
+        start_date_box = self.browser.find_element_by_id('id_start_date')
+        start_date_box.send_keys(start_date)
+        text_box = self.browser.find_element_by_id('id_text')
+        text_box.send_keys(text)
+        type_id_box = self.browser.find_element_by_id('id_type_id')
+        type_id_box.clear() #remove place holder value
+        type_id_box.send_keys(type_id)
+
+        #click save button to be redirected back to main cv page
+        save_button = self.browser.find_element_by_id('id-save-btn')
+        save_button.click()
+
+        current_url = self.browser.current_url
+        self.assertRegex(current_url, '.+/cv/')
+
+        #can see the new entry on the site
+        titles = self.browser.find_elements_by_class_name('entry-title')
+        self.asserTrue(self.checkIsIn(titles, title))
+        dates = self.browser.find_elements_by_class_name('date')
+        self.asserTrue(self.checkIsIn(dates, start_date))
+        content = self.browser.find_elements_by_class_name('text-content')
+        self.asserTrue(self.checkIsIn(content, text))
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -115,6 +114,44 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
